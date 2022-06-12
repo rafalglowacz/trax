c24741a5a296e08dac75dbb199e7e6f5a27e7c9e
Rafał Głowacz <rafalglowacz@gmail.com>
2022-06-12 15:21:15 +0200

    (adding trip): retrieve last trip instead of aggregating miles every time, fix problem with incorrect totals if new trip is older than a previously added one

--- app/Modules/Trips/Http/Controllers/TripController.php
+++ app/Modules/Trips/Http/Controllers/TripController.php
@@ -8,6 +8,7 @@ use App\Modules\Trips\Resources\TripResource as TripResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response as ResponseFacade;
 
 class TripController extends Controller
@@ -19,11 +20,26 @@ class TripController extends Controller
 
     public function store(TripRequest $request): Response
     {
-        $data = $request->validated() + [
-                'total' => $request->user()->trips()->sum('miles') + $request->validated()['miles'],
-            ];
+        DB::transaction(function () use ($request) {
+            $user = $request->user();
 
-        $request->user()->trips()->create($data);
+            $previousTrip = $user->trips()->where('date', '<=', $request->validated()['date'])->first();
+
+            $data = $request->validated() + [
+                    'total' => ($previousTrip->total ?? 0) + $request->validated()['miles'],
+                ];
+
+            $newTrip = $user->trips()->create($data);
+
+            $user->trips()->where('date', '>', $newTrip->date)->update([
+                // MySQL will automatically overwrite this column with current timestamp if we don't specify it.
+                // It can be prevented in MySQL configuration, but I forgot how and couldn't find the solution quickly
+                // so this is a workaround.
+                'date' => DB::raw('date'),
+
+                'total' => DB::raw('total + ' . intval($newTrip->miles)),
+            ]);
+        });
 
         return ResponseFacade::make();
     }
